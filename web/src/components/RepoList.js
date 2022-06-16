142e138711738238d80c6ece15affa037c01e051
user <collie@hotmail.co.uk>
2022-06-15 23:34:28 -0300

    web - add buttons for each language type returned, filter list based on language type

--- web/src/components/RepoList.js
+++ web/src/components/RepoList.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 
 export default function RepoList() {
   const [data, setData] = useState();
+  const [language, setLanguage] = useState();
+  const [filteredData, setFilteredData] = useState();
 
   useEffect(() => {
     axios
@@ -12,22 +14,56 @@ export default function RepoList() {
         },
       })
       .then((response) => {
+        const list = response.data.map((item) => item.language);
+        const buttonList = list.filter((item, index) => {
+          return list.indexOf(item) === index;
+        });
+        setLanguage(buttonList);
         setData(response.data);
       });
   }, []);
   console.log('data: ', data);
+  console.log('language: ', data);
+
+  const showAll = () => {
+    setFilteredData(data);
+  };
+  const showSelection = (item) => {
+    const filt = data.filter((el) => el.language === item);
+    console.log('filt:: ', filt);
+    setFilteredData(filt);
+  };
+
+  console.log('filtered data:: ', filteredData);
   return (
     <div>
       <h1>Silver Orange</h1>
-      {data &&
-        data.map((item) => (
-          <div key={item.id}>
-            <p>{item.name}</p>
-            <p>{item.description}</p>
-            <p>{item.language}</p>
-            <p>{item.forks}</p>
-          </div>
-        ))}
+      <div>
+        <button type="button" onClick={() => showAll()}>
+          Show All
+        </button>
+        {language &&
+          language.map((item) => (
+            <button
+              type="button"
+              key={item.id}
+              onClick={() => showSelection(item)}
+            >
+              {item}
+            </button>
+          ))}
+      </div>
+      <>
+        {filteredData &&
+          filteredData.map((item) => (
+            <div key={item.id}>
+              <p>{item.name}</p>
+              <p>{item.description}</p>
+              <p>{item.language}</p>
+              <p>{item.forks}</p>
+            </div>
+          ))}
+      </>
     </div>
   );
 }
