9d010638eb9e4bddbe1953206110aad6b4d14ced
user <collie@hotmail.co.uk>
2022-06-15 22:10:13 -0300

    return data from github request, where fork is false

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Router, Request, Response } from 'express';
 
 export const repos = Router();
@@ -8,5 +9,19 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  await axios
+    .get('https://api.github.com/users/silverorange/repos', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response: any) => {
+      const repoData = response.data;
+      const json = repoData.filter((item: any) => item.fork === false);
+      res.send(json);
+    })
+    .catch((error:any) => {
+      console.error(error);
+      res.send(error);
+    });
 });
